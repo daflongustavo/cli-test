9600022baa9c36ee8177567624ca1566c4d9e93d
daflongustavo <gustavodaflon@seventechnologies.cloud>
2023-12-27 11:32:13 -0300

    updated gut.rb

--- gut.rb
+++ gut.rb
@@ -1,7 +1,7 @@
 class Gut < Formula
   desc "My homebrew version for Example CLI"
   homepage "https://seusite.com/meucli"
-  url "https://github.com/daflongustavo/cli-test/raw/main/dist/test-gut-1.0.0.tar.gz"
+  url "https://github.com/daflongustavo/cli-test/raw/main/dist/gut.tar.gz"
   sha256 "84005073379ef8c176bc1363294e0cfbdd1d690df05263cc1e2c61db68f0cda0"
 
   depends_on "python"
