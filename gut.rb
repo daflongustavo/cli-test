ecde281673268b060665b082b83f7b4e32c1dda2
daflongustavo <gustavodaflon@seventechnologies.cloud>
2023-12-27 12:13:04 -0300

    small fixes on gut,.rb and setup.py

--- gut.rb
+++ gut.rb
@@ -1,15 +1,16 @@
 class Gut < Formula
+  include Language::Python::Virtualenv
+
   desc "My homebrew version for Example CLI"
   homepage "https://seusite.com/meucli"
-  url "https://github.com/daflongustavo/cli-test/raw/main/dist/gut.tar.gz"
-  sha256 "84005073379ef8c176bc1363294e0cfbdd1d690df05263cc1e2c61db68f0cda0"
+  url "https://github.com/daflongustavo/cli-test/raw/main/dist/gut-1.0.0.tar.gz"
+  sha256 "079308f29b45ba49cee24fa068e730bd629b4c986ba46c01b6001ade87e31618"
 
   depends_on "python"
 
   def install
-    bin.install "gut.py" => "gut"
+    virtualenv_install_with_resources
   end
-  
 
   test do
     system "#{bin}/gut", "--version"
--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='test-gut',
+    name='gut',
     version='1.0.0',
     py_modules=['gut'],
-    install_requires=['Click', ],
-    entry_points={
-        'console_scripts': [
-            'gut = gut:cli'
-        ]
-    })
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        gut=gut:cli
+    '''
+)
