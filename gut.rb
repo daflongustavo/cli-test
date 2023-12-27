2fa8e315221972d14cece95b0c35f5dcae6b9443
daflongustavo <gustavodaflon@seventechnologies.cloud>
2023-12-27 11:15:30 -0300

    added files for testing homebrew locally

--- gut.rb
+++ gut.rb
@@ -1,17 +1,17 @@
-class Meucli < Formula
-    desc "My homebrew version for Example CLI"
-    homepage "https://seusite.com/meucli"
-    url "https://github.com/seuusuario/meucli/archive/v1.0.0.tar.gz"
-    sha256 "hash_do_arquivo.tar.gz"
-  
-    depends_on "python"
-  
-    def install
-      bin.install "meucli"
-    end
-  
-    test do
-      system "#{bin}/meucli", "--version"
-    end
+class Gut < Formula
+  desc "My homebrew version for Example CLI"
+  homepage "https://seusite.com/meucli"
+  url "https://github.com/daflongustavo/cli-test/raw/main/dist/test-gut-1.0.0.tar.gz"
+  sha256 "84005073379ef8c176bc1363294e0cfbdd1d690df05263cc1e2c61db68f0cda0"
+
+  depends_on "python"
+
+  def install
+    bin.install "gut.py" => "gut"
   end
   
+
+  test do
+    system "#{bin}/gut", "--version"
+  end
+end
